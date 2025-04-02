6135e8585ba877224ec5b3d7554987267e01f10f
Alvaro Cabrera <alvaro.cabrera@cuidum.com>
2025-04-02 18:22:26 +0200

    guardadoFinal

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="container">
+        <h1>Lama dev</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
